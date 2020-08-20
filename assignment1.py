7f49c82bf892143de211b9f781bd7da86888f12b
Haider Ali <haiderali@Apples-iMac.local>
2020-08-20 22:16:15 +0500

    tags removed

--- assignment1.py
+++ assignment1.py
@@ -1,8 +1,12 @@
 import urllib.request
+import re
 
 response = urllib.request.urlopen("https://www.york.ac.uk/teaching/cws/wws/webpage1.html")
 html = response.read().decode('utf-8')
 
+clean = re.compile('<.*?>')
+text = re.sub(clean, '',html)
+print(text.strip())
 
-with open("scrapint-text", 'a') as f:
-    f.write(html)
+with open("scraping-text.txt", 'w') as f:
+    f.write(text.strip())
